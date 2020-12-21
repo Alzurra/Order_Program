5e31a693ffafd9bb3fedc1b3e1a96099892d11a9
Thiago <thiagosilva270292@gmail.com>
2020-12-21 12:01:57 -0300

    Projeto Java Eclipse

---
+++ src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+		System.out.println(order);
+
+		OrderStatus os1 = OrderStatus.DELIVERED;
+
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+		OrderStatus os3 = OrderStatus.PROCESSING;
+
+		OrderStatus os4 = OrderStatus.valueOf("PROCESSING");
+
+		System.out.println(os1);
+		System.out.println(os2);
+		System.out.println(os3);
+		System.out.println(os4);
+
+	}
+
+}
